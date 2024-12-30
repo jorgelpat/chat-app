f18358d80abf3b1ee8895cf9da772c1bb6cf654b
jorgelpat <156463233+jorgelpat@users.noreply.github.com>
2024-12-30 16:48:51 -0500

    ui design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+const Conversations = () => {
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+    </div>
+  )
+}
+
+export default Conversations
+
+
+// STARTER CODE FOR SNIPPET
+// import React from 'react'
+// import Conversation from './Conversation'
+
+// const Conversations = () => {
+//   return (
+//     <div className='py-2 flex flex-col overflow-auto'>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//     </div>
+//   )
+// }
+
+// export default Conversations
